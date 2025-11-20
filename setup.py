8580a94f56e22861c1907e175c979ba550286d24
Abhay Pratap <at603251@gmail.com>
2025-11-21 00:02:25 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import find_packages,setup
 from typing import List
-
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path:str) -> List[str]:
     '''
     this function will return the list of requirements
     '''
 
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","")for req in requirements]
