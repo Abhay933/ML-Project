5842d4bd80dbc84fb7d5acde0d025fa49960c384
Abhay Pratap <at603251@gmail.com>
2026-01-16 20:20:12 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path:str) -> List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='krish',
+author='Badal',
 author_email='at603251@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
